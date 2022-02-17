bd3a8114e7b7bb3d9cf663b13d477801d6bf3701
developerhr000 <80412774+developerhr000@users.noreply.github.com>
2022-02-17 19:00:32 +0600

    2nd done

--- js/app.js
+++ js/app.js
@@ -14,12 +14,21 @@ const percentageBtn = document.getElementById('percentage-btn');
 const savingAmount = document.getElementById('saving-amount');
 const remainingBalance = document.getElementById('remaining-balance');
 
-function getInput(input){
+function getInput(input) {
     const inputName = document.getElementById(input);
     const inputValue = parseFloat(inputName.value);
     return inputValue;
 }
+function getText(inner) {
+    const text = document.getElementById(inner);
+    const textValue = parseFloat(text.innerText);
+    return textValue;
+}
 
-document.getElementById('click', function(){
-    
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    const costFood = getInput('input-food');
+    const costRent = getInput('input-rent');
+    const costClothes = getInput('input-clothes');
+    const totalCost = costFood + costRent + costClothes;
+    totalExpenses.innerText = totalCost;
 });
